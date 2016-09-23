3775b538b315f1f6e391ee42275a869849aa2545
ms <ms.com>
2016-09-23 17:19:21 +0530

    changes after security fix

--- oAuthServer/src/main/java/demo/Application.java
+++ oAuthServer/src/main/java/demo/Application.java
@@ -1,12 +1,17 @@
 package demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @SpringBootApplication
 public class Application {
@@ -21,15 +26,30 @@ public class Application {
 	@Configuration
 	@EnableAuthorizationServer
 	protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+	    
+	    @Autowired
+	    private AuthenticationManager authenticationManager;
+	    
+	    @Bean
+	    public JwtAccessTokenConverter accessTokenConverter() {
+	        return new JwtAccessTokenConverter();
+	    }
 
+	    @Override
+	    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+	        endpoints
+	            .authenticationManager(this.authenticationManager)
+	            .accessTokenConverter(accessTokenConverter());
+	    }
 		@Override
 		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 			clients.inMemory()
 				.withClient("client")
-				.secret("secret")
-				.authorizedGrantTypes("password","authorization_code","refresh_token")
+				.secret("password")
+				.authorities("ROLE_TRUSTED_CLIENT")
+				.authorizedGrantTypes("implicit","password","authorization_code","refresh_token","client_credentials")
 				.scopes("seeTheMessage","doSomethingElse")
-//				.accessTokenValiditySeconds(30)
+			   //.accessTokenValiditySeconds(30)
 				;
 		}
 
@@ -38,6 +58,7 @@ public class Application {
 		public void configure(AuthorizationServerSecurityConfigurer oauthServer)	throws Exception {
 			oauthServer
 				.tokenKeyAccess("permitAll()")
+				.allowFormAuthenticationForClients()
 				.checkTokenAccess("isAuthenticated()");
 		}
 
